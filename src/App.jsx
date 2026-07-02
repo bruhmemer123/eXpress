decfd01d12da0bd056698a4323dc8d6d0f91589c
Manan Gohil <manangohil2357@gmail.com>
2026-07-02 17:26:19 +0530

    added star bg

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Team from './pages/Team';
 import Contact from './pages/Contact';
 import Articles from './pages/Articles';
 import Previous_Events from './pages/Previous_Events';
+import Starfield from './components/Starfield';
 import './index.css';
 
 export default function App() {
@@ -16,15 +17,16 @@ export default function App() {
       <div className="flex flex-col min-h-screen bg-gray-900 text-white">
         <Header />
         <main className="flex-grow pt-20">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/expresso" element={<Expresso />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/articles" element={<Articles />} />
-            <Route path="/previous-events" element={<Previous_Events />} />
-          </Routes>
+          <Starfield/>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/expresso" element={<Expresso />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/articles" element={<Articles />} />
+              <Route path="/previous-events" element={<Previous_Events />} />
+            </Routes>
         </main>
         <Footer />
       </div>
