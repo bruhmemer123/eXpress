d043c1f162ee0a56ab132cca046d8da88743dc57
Bhavya-009 <bhavyalakhani10@gmail.com>
2026-07-03 16:24:12 +0530

    Fix navigation bug & Add Back to Top button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import BackToTopButton from './components/BackToTopButton';
 import Home from './pages/Home';
 import About from './pages/About';
 import Expresso from './pages/Expresso';
@@ -11,24 +13,38 @@ import Previous_Events from './pages/Previous_Events';
 import Starfield from './components/Starfield';
 import './index.css';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen text-white">
-        <Starfield/>
-        <Header />
-        <main className="flex-grow pt-0 md:pt-0">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/expresso" element={<Expresso />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/articles" element={<Articles />} />
-            <Route path="/previous-events" element={<Previous_Events />} />
-          </Routes>
-        </main>
-        <Footer />
+      <ScrollToTop />
+      <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+        <div className="flex flex-col min-h-screen text-white">
+          <Starfield />
+          <Header />
+          <main className="flex-grow pt-0 md:pt-0">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/expresso" element={<Expresso />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/articles" element={<Articles />} />
+              <Route path="/previous-events" element={<Previous_Events />} />
+            </Routes>
+          </main>
+          <BackToTopButton />
+          <Footer />
+        </div>
       </div>
     </Router>
   );
